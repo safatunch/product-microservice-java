75662cd5bf78e5c203750cbed56c2c3bc0ff0a61
Safa Tunç <safa.dev80@gmail.com>
2022-04-09 22:35:18 +0300

    Still trying to make it work

--- src/main/java/com/sha/productservice/ProductServiceApplication.java
+++ src/main/java/com/sha/productservice/ProductServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
+@PropertySource("classpath:application-${spring.profiles.active:prod}.properties")
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
