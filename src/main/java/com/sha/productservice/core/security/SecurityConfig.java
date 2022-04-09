e2bacee15bd410ead939a966d4c5f2eac287cacd
Safa Tunç <safa.dev80@gmail.com>
2022-04-09 13:49:36 +0300

    Security Update & Incorrect Prop Fix

--- src/main/java/com/sha/productservice/core/security/SecurityConfig.java
+++ src/main/java/com/sha/productservice/core/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.sha.productservice.core.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,10 +10,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${service.security.secure-key-username}")
+    String Username;
+    @Value("${service.security.secure-key-password}")
+    String Password;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder pwEncoder = new BCryptPasswordEncoder();
-        auth.inMemoryAuthentication().passwordEncoder(pwEncoder).withUser("1").password(pwEncoder.encode("1")).roles("USER");
+        auth.inMemoryAuthentication().passwordEncoder(pwEncoder).withUser(Username).password(pwEncoder.encode(Password)).roles("USER");
     }
 
     @Override
